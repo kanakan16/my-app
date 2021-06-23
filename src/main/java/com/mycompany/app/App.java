e7d6325f29abf4024d8694dbfccc90b727c53992
deepak <hello@deepak.com>
2021-06-23 17:39:41 +0530

    my-app is a sample java pgm

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,24 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+     String first = "Java";
+    String second = "Python";
+    String third = "JavaScript";
+
+    
+    System.out.println(first);   // print Java
+    System.out.println(second);  // print Python
+    System.out.println(third);   // print JavaScript
+        
+    }
+}
+
+
+
